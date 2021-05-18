4501f0e4f5980d9552fb706b42b45d9026ae8d69
David Röger <david@codeatelier.com>
2021-05-18 19:17:04 +0200

    Design tokens

---
+++ main.js
@@ -0,0 +1,95 @@
+
+const fs = require("fs");
+const fetch = require('node-fetch');
+
+const styleDictionary = require('style-dictionary').extend('./config.json');
+
+const del = require('del');
+
+
+let figmaTree;
+let designTokens;
+
+// get colors from styles artboard
+function getColors(stylesArtboard) {
+    const colors = {};
+    const colorsArtboard = stylesArtboard.filter(style => {
+        return style.name === "Colors";
+    })[0].children;
+
+    colorsArtboard.map(color => {
+        function rgba(val) {
+            return Math.floor(val * 255);
+        }
+
+        // convert fill to rgba
+        let fill = color.fills[0];
+        console.log(fill.opacity);
+
+        let r = rgba(fill.color.r)
+        let g = rgba(fill.color.g)
+        let b = rgba(fill.color.b)
+        let a = fill?.opacity ? fill.opacity.toFixed(2) : '1';
+
+        colors[color.name] = {
+            value: `rgba(${r}, ${g}, ${b}, ${a})`,
+            type: "color"
+        }
+    });
+    return colors;
+}
+
+// get figma json
+figmaApiToken = "189840-58838f90-2466-48a1-9634-2006f1332482"
+figmaId = "kGm31xFt0PUcFsiqSzdwk0"
+figmaNodes = ["13%3A2'"]
+async function getFigmaTree() {
+    const query = "https://api.figma.com/v1/files/" + figmaId;
+    const result = await fetch(query, {
+        method: "GET",
+        headers: {
+            "X-Figma-Token": figmaApiToken
+        }
+    });
+    figmaTree = await result.json();
+}
+
+function setDesignTokens() {
+    // filter for styles page
+    const stylesArtboard = figmaTree.document.children.filter(item => {
+        return item.name === "Styles";
+    })[0].children;
+
+    designTokens = {
+        colors: {}
+    };
+
+    // populate design tokens
+    designTokens.colors = getColors(stylesArtboard)
+}
+
+async function writeJsonFile() {
+    await fs.writeFile("tokens.json", JSON.stringify(designTokens, null, 2), err => {
+        if (err) throw err; 
+    });
+}
+
+
+// run script
+async function run() {
+
+    await getFigmaTree();
+
+    setDesignTokens();
+
+    await writeJsonFile();
+
+    await del('build');
+
+    styleDictionary.buildAllPlatforms();
+
+    console.log("--- All Done ---");
+}
+
+run()
+
