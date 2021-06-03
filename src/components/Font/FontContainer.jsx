5e1848e8222811213558e6e49399d3c9c291efcc
David Röger <david@codeatelier.com>
2021-06-03 17:37:12 +0200

    Update locale to en

--- src/components/Font/FontContainer.jsx
+++ src/components/Font/FontContainer.jsx
@@ -5,7 +5,7 @@ import { Container, FontElement } from '../index'
 export function FontContainer( { fonts } ) {
 
     return <Container>
-        <h2 className="text-xl mb-md">Typographie</h2>
+        <h2 className="text-xl mb-md">Typography</h2>
         <div className="grid gap-sm md:gap-md lg:gap-lg xl:gap-xl">
             { Object.keys(fonts).map(font => (
                 <FontElement name={ font } font={ fonts[font] } key={ font }></FontElement>
