a9d327fd68e89187493a3b91b1e2cea7d998154c
David Röger <david@codeatelier.com>
2021-06-03 23:08:33 +0200

    specify imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { Nav, ColorContainer, FontContainer, SpacingContainer, GridContainer, BorderContainer, BreakPointContainer } from './Components/index'
-import { Index } from './Layouts/index'
+import { Nav, ColorContainer, FontContainer, SpacingContainer, GridContainer, BorderContainer, BreakPointContainer } from './Components/index.js'
+import { Index } from './Layouts/index.js'
 import tokens from 'dr066-ba-design-token-builder/web'
 
 let colors = tokens.colors;
