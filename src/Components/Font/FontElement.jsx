80f2110a6a2678b6cd21d1986fb01a5a3fab3f40
David Röger <david@codeatelier.com>
2021-06-04 21:45:53 +0200

    Make copy point more visible

--- src/Components/Font/FontElement.jsx
+++ src/Components/Font/FontElement.jsx
@@ -11,7 +11,7 @@ export function FontElement( { name, font }) {
         <div className="bg-white">
             <input onClick={(e) => e.stopPropagation()} style={{fontSize: size, fontFamily: family}} className="w-full bg-gray-100 focus:outline-none ring-black focus:ring p-sm" defaultValue="Lorem Ipsum"/>
             <div className="m-sm">
-                <CopyToClipBoard text={`font-${family} text-${name} text-leading-${name}`} bg="bg-gray-100">
+                <CopyToClipBoard text={`font-${family} text-${name} text-leading-${name}`} bg="bg-gray-300">
                     <p className="mt-xs">{ name }</p>
                     <p className="text-sm leading-sm text-gray-500"> { size } / { lineHeight } </p>
                     <p className="text-sm leading-sm text-gray-500">{ family }</p> 
